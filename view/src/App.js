2d0a729676fd2195f97be7a868d4d361c6b40c22
mounish <mounishr@gmail.com>
2020-05-22 03:00:43 +0530

    final

--- view/src/App.js
+++ view/src/App.js
@@ -1,17 +1,35 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-import login from './components/login';
+import login from './pages/login';
+import signup from './pages/signup';
+import home from './pages/home';
+import { ThemeProvider as MuiThemeProvider } from '@material-ui/core/styles';
+import createMuiTheme from '@material-ui/core/styles/createMuiTheme';
 
+const theme = createMuiTheme({
+	palette: {
+		primary: {
+			light: '#33c9dc',
+			main: '#FF5722',
+			dark: '#d50000',
+			contrastText: '#fff'
+		}
+	}
+});
 
 const App = () => {
   return (
+    <MuiThemeProvider theme={theme}>
     <Router>
       <div>
         <Switch>
           <Route exact path='/login' component={login}/>
+          <Route exact path="/signup" component={signup}/>
+          <Route exact path="/" component={home}/>
         </Switch>
       </div>
     </Router>
+    </MuiThemeProvider>
   );
 }
 export default App;
